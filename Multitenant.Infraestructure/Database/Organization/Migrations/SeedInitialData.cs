803b28b52e91baee508ee8004a9006420d3d87d9
acano <alexander_cano@cw.groups360.com>
2023-10-22 23:23:02 -0600

    Fix Seeder data

--- Multitenant.Infraestructure/Database/Organization/Migrations/SeedInitialData.cs
+++ Multitenant.Infraestructure/Database/Organization/Migrations/SeedInitialData.cs
@@ -10,13 +10,14 @@ public class SeedInitialData : Migration
     {
         var organization1Guid = Guid.NewGuid();
         var user1 = Guid.NewGuid();
-        var nameOrganization1 = "master";
+        var masterTenant = "master-default";
+        var nameOrganization1 = "tenant master";
         
         Insert.IntoTable(nameof(Organization))
-            .Row(new { Id = organization1Guid, Name = nameOrganization1 });
+            .Row(new { Id = organization1Guid, Name = nameOrganization1,SlugTenant=masterTenant});
 
         Insert.IntoTable(nameof(User))
-            .Row(new { Id = user1, Name = "master user", Password = "master123" ,OrganizationId =organization1Guid});
+            .Row(new { Id = user1, Name = "master user",Email ="master@localdomain.com", Password = "master123" ,OrganizationId =organization1Guid});
     }
 
     public override void Down()
